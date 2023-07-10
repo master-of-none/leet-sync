4ac4d7f80158e05964f419d6a574c07ba129e2f5
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-07-10 08:33:03 +0530

    Time: 54 ms (49.65%), Space: 16.4 MB (14.55%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -10,8 +10,8 @@ class Solution:
 
         while l1 and l2:
             if l1.val < l2.val:
-                tail.next = l1
-                l1 = l1.next
+               tail.next = l1
+               l1 = l1.next
             else:
                 tail.next = l2
                 l2 = l2.next
@@ -20,8 +20,7 @@ class Solution:
 
         if l1:
             tail.next = l1
-        
-        elif l2:
+        else:
             tail.next = l2
         
         return dummy.next
