8bf4fe6852a329c6ea4a067a83904b94c644dc44
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-07-11 15:09:19 +0530

    Time: 139 ms (14.71%), Space: 22.3 MB (20.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -4,9 +4,9 @@ class Solution:
 
         for s in strs:
             count = [0] * 26
-
+            
             for c in s:
-                count[ord(c) - ord('a')] +=1
+                count[ord(c) - ord('a')] += 1
             
             result[tuple(count)].append(s)
         
