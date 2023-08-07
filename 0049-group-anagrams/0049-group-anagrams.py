3437aeaa97be2cb9ad1b6f90f627b9444462be8e
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-08-07 20:26:01 +0530

    Time: 107 ms (81%), Space: 22.3 MB (9.56%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,7 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         result = defaultdict(list)
-
+        
         for s in strs:
             count = [0] * 26
             
