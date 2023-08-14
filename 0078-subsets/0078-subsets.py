5cc460704d1b314d691e68542d539434b31e2c80
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-08-14 07:25:27 +0530

    Time: 34 ms (96.77%), Space: 16.6 MB (11.54%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,21 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        result = []
         subset = []
 
         def dfs(i):
             if i >= len(nums):
-                res.append(subset.copy())
+                result.append(subset.copy())
                 return
             
-            # Left Side which will be included
             subset.append(nums[i])
             dfs(i+1)
 
-            # Right Side which is not included
             subset.pop()
             dfs(i+1)
 
         dfs(0)
-
-        return res 
+        return result
