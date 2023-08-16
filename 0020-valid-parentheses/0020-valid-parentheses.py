50ccb39990f8f1999bf76b26718aad4e9ec16520
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-08-16 22:10:17 +0530

    Time: 36 ms (89.62%), Space: 16.3 MB (84.93%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,21 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
-        hashmap = {")":"(", "]":"[", "}":"{"}
+      stack = []
+      hashmap = {")": "(", "}":"{", "]":"["}
 
-        for c in s:
-            if c in hashmap:
-                if stack and stack[-1] == hashmap[c]:
-                    stack.pop()
-                
-                else:
-                    return False
-            
-            else:
-                stack.append(c)
-        
-        if not stack:
-            return True
-        else:
+      for c in s:
+        if c in hashmap:
+          if stack and stack[-1] == hashmap[c]:
+            stack.pop()
+          else:
             return False
         
+        else:
+          stack.append(c)
+      
+      return True if not stack else False
