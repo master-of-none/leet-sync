ffa56fecf8ad6016d292d250aac3689bcd8c214f
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-08-16 16:20:03 +0530

    Time: 380 ms (24.28%), Space: 24.4 MB (22.36%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -8,27 +8,38 @@ class Solution:
         res = 0
         visit = set()
 
-        def bfs(r, c):
-            q = collections.deque()
+        # def bfs(r, c):
+        #     q = collections.deque()
+        #     visit.add((r, c))
+        #     q.append((r, c))
+
+        #     while q:
+        #         row, col = q.popleft()
+        #         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+        #         for dr, dc in directions:
+        #             r, c = row + dr, col + dc
+
+        #             if r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r, c) not in visit:
+        #                 q.append((r, c))
+        #                 visit.add((r,c))
+
+        def dfs(r, c):
+            if r not in range(rows) or c not in range(cols) or grid[r][c] == "0" or (r, c) in visit:
+                return
+            
             visit.add((r, c))
-            q.append((r, c))
+            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
-            while q:
-                row, col = q.popleft()
-                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-
-                for dr, dc in directions:
-                    r, c = row + dr, col + dc
-
-                    if r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r, c) not in visit:
-                        q.append((r, c))
-                        visit.add((r,c))
+            for dr, dc in directions:
+                dfs(r + dr, c+ dc)
 
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1" and (r,c) not in visit:
-                    bfs(r, c)
+                    # bfs(r, c)
+                    dfs(r, c)
                     res += 1
         
         return res
