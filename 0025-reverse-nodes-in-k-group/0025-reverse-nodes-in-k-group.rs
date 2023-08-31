06a4b2f1a2752d9a1894e433461716180acb89f6
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-08-31 13:44:00 +0530

    Time: 0 ms (100%), Space: 2.5 MB (10.64%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.rs
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.rs
@@ -16,47 +16,49 @@
 // }
 impl Solution {
     pub fn reverse_k_group(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
-          let mut dummy_head = Some(Box::new(ListNode { val: 0, next: head }));
-        let mut head = dummy_head.as_mut();
+        let mut dummy = Some(Box::new(ListNode {next: head, val: 0 }));
+        let mut cur = dummy.as_mut();
+
         'outer: loop {
-            let mut start = head.as_mut().unwrap().next.take();
+            let mut start = cur.as_mut().unwrap().next.take();
             if start.is_none() {
                 break 'outer;
             }
+
             let mut end = start.as_mut();
             for _ in 0..(k - 1) {
                 end = end.unwrap().next.as_mut();
+
                 if end.is_none() {
-                    head.as_mut().unwrap().next = start;
+                    cur.as_mut().unwrap().next = start;
                     break 'outer;
                 }
             }
+
             let mut tail = end.as_mut().unwrap().next.take();
-            // BEFORE: head -> start -> 123456... -> end   -> tail
-            // AFTER:  head -> end   -> ...654321 -> start -> tail
             let end = Solution::reverse(start, tail);
-            head.as_mut().unwrap().next = end;
+            cur.as_mut().unwrap().next = end;
+
             for _ in 0..k {
-                head = head.unwrap().next.as_mut()
+                cur = cur.unwrap().next.as_mut()
             }
         }
-        dummy_head.unwrap().next
+        dummy.unwrap().next
     }
 
-    #[inline(always)]
     fn reverse(
         mut head: Option<Box<ListNode>>,
         tail: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
         let mut prev = tail;
-        let mut current = head;
-        while let Some(mut current_node_inner) = current {
-            let mut next = current_node_inner.next.take();
-            current_node_inner.next = prev.take();
-            prev = Some(current_node_inner);
-            current = next;
+        let mut cur = head;
+
+        while let Some(mut cur_node) = cur {
+            let mut next = cur_node.next.take();
+            cur_node.next = prev.take();
+            prev = Some(cur_node);
+            cur = next
         }
         prev
     }
 }
-    
