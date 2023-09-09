00715afc39c5bf61d1c514d406f66de3b2e7938e
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-09-09 20:00:16 +0530

    Time: 44 ms (74.41%), Space: 16.5 MB (21.45%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        # potential optimization
+        # nums.sort()
+
+        @functools.lru_cache(maxsize = None)
+        def combs(remain):
+            if remain == 0:
+                return 1
+
+            result = 0
+            for num in nums:
+                if remain - num >= 0:
+                    result += combs(remain - num)
+                # potential optimization
+                # else:
+                #     break
+
+            return result
+
+        return combs(target)
