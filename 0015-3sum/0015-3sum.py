53956239d9975a61ce85d7ee475ba8e5525df480
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-09-12 07:43:24 +0530

    Time: 1095 ms (61.08%), Space: 20.5 MB (38.87%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -7,21 +7,25 @@ class Solution:
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             
-            l = i + 1
-            r = len(nums) - 1
+            l = i+1
+            r = len(nums)-1
 
             while l < r:
                 temp = nums[i] + nums[l] + nums[r]
 
                 if temp < 0:
                     l += 1
+                
                 elif temp > 0:
                     r -= 1
+                
                 else:
-                    res.append([nums[i],nums[l], nums[r]])
+                    res.append([nums[i], nums[l], nums[r]])
                     l += 1
                     r -= 1
-
-                    while l < len(nums) and nums[l] == nums[l-1]:
+                
+                    while l < r and nums[l] == nums[l-1]:
                         l += 1
+        
         return res
+        
