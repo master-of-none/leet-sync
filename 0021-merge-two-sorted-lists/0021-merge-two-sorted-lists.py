67818a4b23308fce2c0b0b6c1a89399f30210ec9
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-09-13 07:05:02 +0530

    Time: 41 ms (78.11%), Space: 16.5 MB (10.46%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -12,16 +12,17 @@ class Solution:
             if l1.val < l2.val:
                 cur.next = l1
                 l1 = l1.next
+            
             else:
                 cur.next = l2
                 l2 = l2.next
-        
+            
             cur = cur.next
         
         if l1:
             cur.next = l1
-
-        elif l2:
+        
+        else:
             cur.next = l2
         
         return dummy.next
