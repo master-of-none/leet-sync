4b8f0e48f34896ab7738e7ea201f855c54794e29
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-09-13 22:05:08 +0530

    Time: 86 ms (63.91%), Space: 17.9 MB (50.69%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -3,8 +3,8 @@ class Solution:
         index = 1
 
         for i in range(1, len(nums)):
-            if nums[i - 1] != nums[i]:
+            if nums[i-1] != nums[i]:
                 nums[index] = nums[i]
                 index += 1
-
+        
         return index
