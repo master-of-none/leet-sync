2ae3b61ef68b14af128ad27ad7bf947b5d8a1e21
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-09-13 07:48:00 +0530

    Time: 359 ms (21.26%), Space: 24.3 MB (22.79%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,44 +5,28 @@ class Solution:
         
         rows = len(grid)
         cols = len(grid[0])
-        res = 0
-        visit = set()
+        visited = set()
+        count = 0
 
-        # def bfs(r, c):
-        #     q = collections.deque()
-        #     visit.add((r, c))
-        #     q.append((r, c))
+        def bfs(r, c):
+            q = collections.deque()
+            visited.add((r, c))
+            q.append((r, c))
 
-        #     while q:
-        #         row, col = q.popleft()
-        #         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-
-        #         for dr, dc in directions:
-        #             r, c = row + dr, col + dc
-
-        #             if r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r, c) not in visit:
-        #                 q.append((r, c))
-        #                 visit.add((r,c))
-
-        def dfs(r, c):
-            if r not in range(rows) or c not in range(cols) or grid[r][c] == "0" or (r, c) in visit:
-                return
-            
-            visit.add((r, c))
-            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-
-            for dr, dc in directions:
-                dfs(r + dr, c+ dc)
-
-
+            while q:
+                row, col = q.popleft()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                for dr, dc in directions:
+                    if (row+dr) in range(rows) and (col+dc) in range(cols) and grid[row+dr][col+dc] == "1" and (row+dr, col+dc) not in visited:
+                        q.append((row+dr, col+dc))
+                        visited.add((row+dr, col + dc))
+        
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r,c) not in visit:
-                    # bfs(r, c)
-                    dfs(r, c)
-                    res += 1
-        
-        return res
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    bfs(r, c)
+                    count += 1
+        return count
 
-              
+
         
