629d4b1c6462ffb2c71040690d34cb87ff9a0e25
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-09-13 22:12:24 +0530

    Time: 32 ms (91.51%), Space: 16.3 MB (52.73%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -3,9 +3,9 @@ class Solution:
         i = len(s) - 1
         res = 0
 
-        while s[i] == " ":
+        while i > 0 and s[i] == " ":
             i -= 1
-        
+
         while i >= 0 and s[i] != " ":
             res += 1
             i -= 1
