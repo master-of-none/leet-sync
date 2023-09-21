d839adf2604d4d9f0366bc20f97cd2b6977dbc5f
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-09-21 15:23:28 -0700

    Time: 85 ms (83.71%), Space: 16.5 MB (68.59%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,31 +1,12 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        m, n = len(nums1), len(nums2)
-        p1, p2 = 0, 0
+        nums1.extend(nums2)
+        nums1.sort()
+
+        if len(nums1) % 2 != 0:
+            return float(nums1[len(nums1)//2])
         
-        # Get the smaller value between nums1[p1] and nums2[p2].
-        def get_min():
-            nonlocal p1, p2
-            if p1 < m and p2 < n:
-                if nums1[p1] < nums2[p2]:
-                    ans = nums1[p1]
-                    p1 += 1
-                else:
-                    ans = nums2[p2]
-                    p2 += 1
-            elif p2 == n:
-                ans = nums1[p1]
-                p1 += 1
-            else:
-                ans = nums2[p2]
-                p2 += 1
-            return ans
-        
-        if (m + n) % 2 == 0:
-            for _ in range((m + n) // 2 - 1):
-                _ = get_min()
-            return (get_min() + get_min()) / 2
         else:
-            for _ in range((m + n) // 2):
-                _ = get_min()
-            return get_min()
+            m = nums1[len(nums1)//2] + nums1[(len(nums1)// 2) - 1]
+            return float(m / 2)
+        return 0
