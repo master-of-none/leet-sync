a70c0626cf1052958ae32ad0366d59e87b34e672
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-09-26 11:18:18 -0700

    Time: 120 ms (11.92%), Space: 22.1 MB (24.98%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -7,7 +7,7 @@ class Solution:
 
             for c in s:
                 count[ord(c) - ord("a")] += 1
-            
+        
             res[tuple(count)].append(s)
         
         return res.values()
