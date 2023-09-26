7b2a956f12197767b429d7875ceccaee6f97a03c
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-09-26 11:26:41 -0700

    Time: 46 ms (12.59%), Space: 16.2 MB (76.29%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -7,9 +7,12 @@ class Solution:
             if c in hashmap:
                 if stack and stack[-1] == hashmap[c]:
                     stack.pop()
+                
                 else:
                     return False
+                
             else:
                 stack.append(c)
         
         return True if not stack else False
+        
