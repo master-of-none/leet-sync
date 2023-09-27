d4a27e81cd64b3008a6df544a7d57fd184838b6a
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-09-27 12:45:45 -0700

    Time: 102 ms (56.92%), Space: 16.2 MB (93.48%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -6,14 +6,14 @@ class Solution:
 
         for r in range(9):
             for c in range(9):
-                if board[r][c] == '.':
+                if board[r][c] == ".":
                     continue
                 
-                if board[r][c] in row[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3,c//3)]:
+                if board[r][c] in row[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3, c//3)]:
                     return False
                 
                 row[r].add(board[r][c])
                 cols[c].add(board[r][c])
                 squares[(r//3, c//3)].add(board[r][c])
-        
+
         return True
