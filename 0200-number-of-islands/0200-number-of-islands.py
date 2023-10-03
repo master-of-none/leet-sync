28dde7b59759ce41e94a6809f43938ad4073f7d0
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-10-02 22:31:23 -0700

    Time: 362 ms (13.15%), Space: 24.2 MB (27.35%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,31 +2,32 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-        
-        rows = len(grid)
-        cols = len(grid[0])
-        visited = set()
-        count = 0
 
-        def bfs(r, c):
-            q = collections.deque()
-            visited.add((r, c))
-            q.append((r, c))
+        rows, cols = len(grid), len(grid[0])
+        visited=set()
+        islands=0
 
+        def bfs(r,c):
+            q = deque()
+            visited.add((r,c))
+            q.append((r,c))
+           
             while q:
-                row, col = q.popleft()
-                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-                for dr, dc in directions:
-                    if (row+dr) in range(rows) and (col+dc) in range(cols) and grid[row+dr][col+dc] == "1" and (row+dr, col+dc) not in visited:
-                        q.append((row+dr, col+dc))
-                        visited.add((row+dr, col + dc))
-        
+                row,col = q.popleft()
+                directions= [[1,0],[-1,0],[0,1],[0,-1]]
+               
+                for dr,dc in directions:
+                    r,c = row + dr, col + dc
+                    if (r) in range(rows) and (c) in range(cols) and grid[r][c] == '1' and (r ,c) not in visited:
+                       
+                        q.append((r , c ))
+                        visited.add((r, c ))
+
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visited:
-                    bfs(r, c)
-                    count += 1
-        return count
+               
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    bfs(r,c)
+                    islands +=1 
 
-
-        
+        return islands
