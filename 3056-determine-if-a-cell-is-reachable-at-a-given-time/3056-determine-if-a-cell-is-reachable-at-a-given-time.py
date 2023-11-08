bd32af3a966bca5da9810df719c04915933d7a0d
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2023-11-07 21:03:23 -0800

    Time: 52 ms (7.77%), Space: 16.1 MB (99.28%) - LeetHub

---
+++ 3056-determine-if-a-cell-is-reachable-at-a-given-time/3056-determine-if-a-cell-is-reachable-at-a-given-time.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isReachableAtTime(self, sx: int, sy: int, fx: int, fy: int, t: int) -> bool:
+        width = abs(sx - fx)
+        height = abs(sy - fy)
+        if width == 0 and height == 0 and t == 1:
+            return False
+        return t >= max(width, height)
